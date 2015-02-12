3a542f553dc063a8db20821abc1e29a42e9b5b14
Andreas Karlsson <sprawl@onlajn.nu>
2015-02-12 22:56:16 +0100

    fixed bugs

--- test/auto_bus.js
+++ test/auto_bus.js
@@ -13,14 +13,30 @@ describe('AutoBus', function() {
 				done();
 			}
 		});
-		it('receive message', function(done) {
+		it('receive message on pub channel', function(done) {
 			var bus = new AutoBus();
 			bus.join('test', function(m) {
 				done();
 			});
 			bus.publish('test', {x:1});
 		});
-
+		it('send with one subscriber', function(done) {
+			var bus = new AutoBus();
+			bus.join('test', function(m) {
+				done();
+			});
+			bus.send('test', {x:1});
+		});
+		it('send fail with two subscribers', function(done) {
+			var bus = new AutoBus();
+			bus.join('test', function(m) {
+				done();
+			});
+			bus.join('test', function(m) {
+				done();
+			});
+			bus.send('test', {x:1});
+		});
 	});
 
 });
